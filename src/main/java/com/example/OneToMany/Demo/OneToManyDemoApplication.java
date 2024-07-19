9971b55cf9c53703b459d6c7d7c82ff517260a5f
Koushal Kumar <koushalkumar2224@gmail.com>
2024-07-19 23:27:09 +0530

    Deployment

---
+++ src/main/java/com/example/OneToMany/Demo/OneToManyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.OneToMany.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToManyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToManyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OneToMany/Demo/OneToManyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OneToMany.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToManyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
